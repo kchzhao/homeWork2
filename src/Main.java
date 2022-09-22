0315cc386e6f41aa06acc8035e11047b455f1830
kchzhao <kamila.chzhao@gmail.com>
2022-09-21 23:58:31 -0400

    hw3 initial commit

--- src/Main.java
+++ src/Main.java
@@ -15,5 +15,25 @@ public class Main {
         System.out.println(frog);
         frog = frog + 4;
         System.out.println(frog);
+
+        System.out.println();
+        
+        var firstBoxerWeight = 78.2;
+        var secondBoxerWeight = 82.7;
+        var totalWeight = firstBoxerWeight + secondBoxerWeight;
+        System.out.println("Total weight of the boxers is " + totalWeight + " kg.");
+        var weightDifference = secondBoxerWeight - firstBoxerWeight;
+        System.out.println("Weight difference is " + weightDifference + " kg.");
+        var wDifference = secondBoxerWeight % firstBoxerWeight;
+        System.out.println("Weight difference is " + wDifference + " kg.");
+
+        System.out.println();
+
+        var totalTime = 640;
+        var totalTimeSpent = 8;
+        var numEmployees = totalTime / totalTimeSpent;
+        System.out.println("The total number of employees is " + numEmployees + ".");
+
+
     }
 }
